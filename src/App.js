8416ac14575a5cc25ed055b3c8df08cbcabddc4a
Tamás Kis <t.kis502@gmail.com>
2023-08-12 16:42:56 +0200

    start with a square with an X in it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import './App.css';
 
-export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Tic Tac Toe tutorial</h1>
-      </header>
-    </div>
-  );
+export default function Square() {
+  return <button className="square">X</button>;
 }
 
