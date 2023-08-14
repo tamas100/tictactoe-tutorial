bfce32cc5a8f284011f84171b8e76c8959194e98
Tamás Kis <t.kis502@gmail.com>
2023-08-14 17:31:22 +0200

    add early return to handleClick

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-
+    if (squares[i]) return;
     const nextSquares = squares.slice();
     xIsNext ? nextSquares[i] = "X" : nextSquares[i] = "O";
     setSquares(nextSquares);
