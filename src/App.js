731c7412c61923ed787ae8278b6eb56f831f95f3
Tamás Kis <t.kis502@gmail.com>
2023-08-16 10:54:12 +0200

    add Game component

--- src/App.js
+++ src/App.js
@@ -9,16 +9,12 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
+function Board(xIsNext, squares, onPlay) {
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) return;
     const nextSquares = squares.slice();
     xIsNext ? nextSquares[i] = "X" : nextSquares[i] = "O";
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext)
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -64,5 +60,27 @@ export default function Board() {
     }
     return null;
   }
+}
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board xIsNext={xIsNext} squaers={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className='game-info'>
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
 }
 
