f28023f7feedf0df70b631fb47770a14d2c9a147
Tamás Kis <t.kis502@gmail.com>
2023-09-03 08:25:52 +0200

    rewrite Board to use two loops to make the squares instead of hardcoding them, use unordered list to display the moves

--- src/App.js
+++ src/App.js
@@ -20,24 +20,25 @@ function Board({ xIsNext, squares, onPlay }) {
   const winner = calculateWinner(squares);
   let status;
   winner ? (status = "Winner: " + winner) : (status = "Next player: " + (xIsNext ? "X" : "O"));
+
+  function MakeBoard() {
+    const boardRows = [];
+    for (let i = 0; i < 3; i++) {
+      const rowSquares = [];
+      for (let j = i * 3; j < i * 3 + 3; j++) {
+        rowSquares.push(
+          <Square value={squares[j]} onSquareClick={() => handleClick(j)} />
+        );
+      }
+      boardRows.push(<div className='board-row'>{rowSquares}</div>);
+    }
+    return boardRows;
+  }
+
   return (
     <>
       <div className='status'>{status}</div>
-      <div className='board-row'>
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div >
-      <div className='board-row'>
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div >
-      <div className='board-row'>
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div >
+      <MakeBoard />
     </>
   );
 
@@ -101,7 +102,7 @@ export default function Game() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className='game-info'>
-        <ol>{moves}</ol>
+        <ul>{moves}</ul>
       </div>
     </div>
   );
